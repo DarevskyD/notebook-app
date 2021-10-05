f4694e44937e380f8b5e94a6b29d349ad3d7118c
Dmitry Darevsky <darevsky.dmitry@gmail.com>
2021-10-05 12:26:12 +0300

    Fix small bug

--- src/components/note-status-filter/note-status-filter.js
+++ src/components/note-status-filter/note-status-filter.js
@@ -4,13 +4,13 @@ import './note-status-filter.css';
 export default class NoteStatusFilter extends Component {
   constructor(props) {
     super(props);
-    this.battons = [
+    this.buttons = [
       {name: 'all', label: 'All'},
       {name: 'like', label: 'Liked'}
     ];
   }
   render() {    
-    const buttons = this.battons.map(({name, label}) => {
+    const buttons = this.buttons.map(({name, label}) => {
       const {filter, onFilterSelect} = this.props;
       const active = filter === name;
       const clazz = active ? 'btn-info' : 'btn-outline-secondary';
